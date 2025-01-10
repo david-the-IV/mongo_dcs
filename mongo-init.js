253ae88ecc9eed9da3ca936f2eea1ca4fedcc402
david-the-IV <gogominelans@gmail.com>
2025-01-10 09:40:45 +0700

    Update mongo-init.js
    fix port

--- mongo-init.js
+++ mongo-init.js
@@ -1,4 +1,4 @@
-db = connect("mongodb://root:rootpassword@localhost:27017/admin");
+db = connect("mongodb://root:rootpassword@localhost:27020/admin");
 
 // Create user 'edy'
 db.getSiblingDB('admin').createUser({
@@ -22,14 +22,14 @@ db.getSiblingDB('admin').createUser({
 db = db.getSiblingDB('dht22_logs');
 db.createCollection('log_dht');
 
-// Database and collection for Biometrics logs
-db = db.getSiblingDB('biometrics_logs');
-db.createCollection('biometrics');
+// Database and collection for RTSP App logs
+db = db.getSiblingDB('rtsp_app_logs');
+db.createCollection('rtsp_app');
 
 // Database and collection for Tuya App logs
 db = db.getSiblingDB('tuya_app_logs');
 db.createCollection('tuya_app');
 
-// Database and collection for RTSP App logs
-db = db.getSiblingDB('rtsp_app_logs');
-db.createCollection('rtsp_app');
+// Database and collection for Biometrics logs
+db = db.getSiblingDB('biometrics_logs');
+db.createCollection('biometrics');
